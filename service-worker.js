ae13029f4d06c3ddbbb1c70b4bf1dddfeac34b13
NASEER ULLAH <162457348+naseer909@users.noreply.github.com>
2024-07-18 16:25:30 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mynew-portfolio/home/precache-manifest.467e71201db93d9a1fa30f1313635811.js"
+  "/mynew-portfolio/precache-manifest.62d8bd30e899bd8b0929fd7ce6dee16f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mynew-portfolio/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mynew-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
